258417485399f29575498f7139f853bcafb600f0
n0thin4 <118237343+n0thin4@users.noreply.github.com>
2023-10-30 19:31:01 +0000

    im tired man

--- elapsed-time/main.js
+++ elapsed-time/main.js
@@ -2,6 +2,13 @@
 function id(txt){
    return document.getElementById(txt)
 }
+//adding visibility detector to detect when the user leaves the page
+//need to add mouseover events to hide the UI if the users mouse is idle for a while
+document.addEventListener("visibilitychange", () => {
+    if (document.hidden) {
+      hideUnimportant()
+    }
+  });
 var root=document.querySelector(':root')
 function setCVar(varName, val){
    root.style.setProperty(`--${varName}`, val)
@@ -18,16 +25,22 @@ function showUnimportant(){
     clearTimeout(unInpTiming)
     setCVar('unimportant', '1')
     //setting the timing in order to make everything unimportant disappear.
+    console.log(document.getElementsByClassName('unimportant'))
     unInpTiming=setTimeout(function(){
-        setCVar('unimportant', '0')
-        unInpTiming=setTimeout(function(){
-            document.getElementsByClassName('unimportant').forEach(function(e){
-                e.visibility='hidden';
-            })}, 500)
+        hideUnimportant();
     },10000)
-    document.getElementsByClassName('unimportant').forEach(function(e){
-        e.visibility='visible';
-    })
+}
+var idleEvent;
+function idleMouse(){
+    clearTimeout(idleEvent)
+    idleEvent=setTimeout(hideUnimportant, 20000)
+}
+function hideUnimportant(){
+    //hide stuff after 10 seconds
+    setCVar('unimportant', '1')
+    setTimeout(function(){
+        setCVar('unimportant-vis', 'hidden')
+    },500)
 }
 var sideBarOpen=false;
 function toggleSideBar(){
